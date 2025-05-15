fe6a029c050f793cd0222e9760dd6ddbc5d76892
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-15 14:07:08 +0100

    Commit

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-int main(void) {
+int main() {
 
     int n, a = 0, b = 1, c;
     scanf("%d", &n);
-
-    while (a < n)
+    while (a <= n)
     {
-        printf("%d ", a);
+        printf("%d \n", a);
         c = a + b;
         a = b;
         b = c;
--- largest_of_three.c
+++ largest_of_three.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
-
-    int max = a;
-    if (b > max)
-        max = b;
-    if (c > max)
-        max = c;
-
-    printf("%d\n", max);
+    int num1, num2, num3, grand;
+    scanf("%d %d %d", &num1, &num2, &num3);
+    if (num1 > num2 && num1 > num3)
+    {
+        grand = num1;
+    } else if (num2 > num1 && num2 > num3)
+    {
+        grand = num2;
+    } else
+    {
+        grand = num3;
+    }
+    printf("%d\n",grand);
+    
+    
     return 0;
 }
