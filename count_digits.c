f3d288cf90774e5098f3dbdbe18f2bf54bf431b0
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-15 02:25:18 +0100

    Commit 2

--- count_digits.c
+++ count_digits.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-  int n;
+  int n, a;
   int count = 0;
-  printf("Entrer un entier :");
   scanf("%d", &n);
+  a = n;
  
   while (n != 0) {
     n /= 10;
     count++;
   }
 
-  printf("le nombre de chiffre(s) est %d.",count);
+  printf("%d contient %d chiffre(s).", a, count);
 }
