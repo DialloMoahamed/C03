c66cab98895049da1939d6e006098d9ba842fb64
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-16 00:33:06 +0100

    commit

--- largest_of_three.c
+++ largest_of_three.c
@@ -3,16 +3,17 @@
 int main()
 {
     int num1, num2, num3;
+    printf("Entrer trois entiers : ");
     scanf("%d %d %d", &num1, &num2, &num3);
     if (num1 >= num2 && num1 >= num3)
     {
-        printf("Le plus grand est :%d",num1);
+        printf("\nLe plus grand est :%d",num1);
     } else if (num2 >= num1 && num2 >= num3)
     {
-        printf("Le plus grand est :%d",num2);
+        printf("\nLe plus grand est : %d",num2);
     } else
     {
-        printf("Le plus grand est :%d",num3);
+        printf("\nLe plus grand est : %d",num3);
     } 
  
     return 0;
