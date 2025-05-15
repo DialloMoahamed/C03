548ada3cc927163a2ee21389ad97eaa33a80fa84
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-15 03:25:00 +0100

    Commit

--- celsius_to_fahrenheit.c
+++ celsius_to_fahrenheit.c
@@ -1,13 +1,1 @@
-#include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-    double temp, fr;
-
-    scanf("%lf", &temp);
-
-    fr = temp * 9/5 + 32;
-
-    printf("%lf°C équivaut à %lf°F", temp, fr);
-    return 0;
-}
