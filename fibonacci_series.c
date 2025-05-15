e11f6cc3e6f8d18c62f2a4cccd5bb7e7bf48e73f
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-15 19:41:37 +0100

    Commit

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -4,6 +4,7 @@ int main() {
 
     int n, a = 0, b = 1, c;
     scanf("%d", &n);
+    printf("la série de Fibonacci : ");
     for (int i = 1; i < n; i++)
     {
         if (a < n)
--- largest_of_three.c
+++ largest_of_three.c
@@ -6,16 +6,14 @@ int main()
     scanf("%d %d %d", &num1, &num2, &num3);
     if (num1 > num2 && num1 > num3)
     {
-        grand = num1;
+        printf("Le plus grand est : %d\n",num1);
     } else if (num2 > num1 && num2 > num3)
     {
-        grand = num2;
+        printf("Le plus grand est : %d\n",num2);
     } else
     {
-        grand = num3;
-    }
-    printf("Le plus grand est : %d\n",grand);
-    
-    
+        printf("Le plus grand est : %d\n",num3);
+    }    
+    printf("\n");
     return 0;
 }
