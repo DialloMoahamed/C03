bb6d8c74d88b0cb61db19ec643e65c056d45d007
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-15 02:17:45 +0100

    Commit 2

--- count_digits.c
+++ count_digits.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-  long long n;
+  int n;
   int count = 0;
   printf("Entrer un entier :");
-  scanf("%lld", &n);
+  scanf("%d", &n);
  
   while (n != 0) {
     n /= 10;
     count++;
   }
 
-  printf("%d contient %d chiffre(s).",n , count);
+  printf("le nombre de chiffre(s) est %d.",count);
 }
--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-int main() {
-    int n, a = 0, b = 1, c;
-    printf("Entrer un entier :");
-    scanf("%d", &n);
+// int main() {
+//     int n, a = 0, b = 1, c;
+//     printf("Entrer un entier :");
+//     scanf("%d", &n);
 
-    for (int i = 1; i <= n; i++) {
-        if (a <= n)
-        {
-            printf("%d ", a);
-            c = a + b;
-            a = b;
-            b = c;
-        }
-    }
+//     for (int i = 1; i <= n; i++) {
+//         if (a <= n)
+//         {
+//             printf("%d ", a);
+//             c = a + b;
+//             a = b;
+//             b = c;
+//         }
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
