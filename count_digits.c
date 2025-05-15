a6a6341a6dfa5dac0061ad5fc3ac028fa6b2a097
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-15 04:06:24 +0100

    Commit

--- count_digits.c
+++ count_digits.c
@@ -1,15 +1,1 @@
-#include <stdio.h>
 
-int main() {
-  int n, a;
-  int count = 0;
-  scanf("%d", &n);
-  a = n;
- 
-  while (n != 0) {
-    n /= 10;
-    count++;
-  }
-
-  printf("%d contient %d chiffre(s).", a, count);
-}
