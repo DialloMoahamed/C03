71e6a08a0d6a900128602520d36365440a0030f6
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-16 20:12:17 +0100

    Commit

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
+
 int main() {
-  int a = 0, b = 1, c = 0, n;
+  int a = 0, b = 1, c, n;
   scanf("%d", &n);
 
+  // On affiche d'abord le premier terme
+  if (n >= 0) printf("%d ", a);
+  if (n >= 1) printf("%d ", b);
+
+  c = a + b;
   while (c <= n) {
     printf("%d ", c);
     a = b;
     b = c;
     c = a + b;
-  } 
+  }
   return 0;
 }
+
