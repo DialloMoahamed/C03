f952f923430a8caf2be76144d17b6b2225583307
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-16 19:54:26 +0100

    Commit

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -3,12 +3,11 @@ int main() {
     int t1 = 0, t2 = 1, nextTerm = 0, n;
     scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) {
-        if (nextTerm < n)
-            printf("%d ", nextTerm);
-            t1 = t2;
-            t2 = nextTerm;
-            nextTerm = t1 + t2;
-    }
+    while (nextTerm <= n) {
+      printf("%d, ", nextTerm);
+      t1 = t2;
+      t2 = nextTerm;
+      nextTerm = t1 + t2;
+    } 
   return 0;
 }
