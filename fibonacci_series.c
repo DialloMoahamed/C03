2dc4dc8e8e16501d8a5535dd11237a430cbc4e6b
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-16 15:01:26 +0100

    Commit

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+int main() {
+  int t1 = 0, t2 = 1, nextTerm = 0, n;
+  printf("Enter a positive number: ");
+  scanf("%d", &n);
 
-int main()
-{
-    int n, a = 0, b = 1, c;
-    scanf("%d", &n);
-    while (a < n)
-    {
-        printf("%d, ", a);
-        c = a + b;
-        a = b;
-        b = c;
-          
-    }
+  // displays the first two terms which is always 0 and 1
+  printf("Fibonacci Series: %d %d ", t1, t2);
+  nextTerm = t1 + t2;
 
-    return 0;
+  while (nextTerm <= n) {
+    printf("%d ", nextTerm);
+    t1 = t2;
+    t2 = nextTerm;
+    nextTerm = t1 + t2;
+  }
+
+  return 0;
 }
