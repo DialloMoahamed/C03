727b463c274b91742385fc7d35e24d173e00957c
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-16 03:11:24 +0100

    Commit

--- largest_of_three.c
+++ largest_of_three.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-    float num1, num2, num3;
-    scanf("%f %f %f", &num1, &num2, &num3);
+    int num1, num2, num3;
+    scanf("%d %d %d", &num1, &num2, &num3);
+
     if (num1 >= num2 && num1 >= num3)
-        printf("Le plus grand est : %.2f", num1);
+        printf("Le plus grand est : %d\n", num1);
+    else if (num2 >= num1 && num2 >= num3)
+        printf("Le plus grand est : %d\n", num2);
+    else
+        printf("Le plus grand est : %d\n", num3);
 
-    if (num2 >= num1 && num2 >= num3)
-        printf("Le plus grand est : %.2f",num2);
-
-    if (num3 >= num1 && num3 >= num2)
-        printf("Le plus grand est : %.2f",num3);
- 
     return 0;
 }
+
