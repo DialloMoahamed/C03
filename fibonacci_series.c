4968c0774bd072035712549152b3d6a61e22adf5
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-17 02:04:54 +0100

    Commit

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
-int main() {
 
-  int i, n;
+int main(void) {
+    int n, i;
+    int a = 0, b = 1, c;
+    scanf("%d", &n);
 
-  int t1 = 0, t2 = 1;
-
-  int nextTerm = t1 + t2;
-
-  scanf("%d", &n);
-
-  printf("%d %d ", t1, t2);
-
-  for (i = 0; i <= n; i++) {
-    if (t1 <= n)
-    {
-        printf("%d ", nextTerm);
-        t1 = t2;
-        t2 = nextTerm;
-        nextTerm = t1 + t2;
+    for (i = 0; i < n; i++) {
+        printf("%d  ", a);
+        c = a + b;
+        a = b;
+        b = c;
     }
-  }
 
-  return 0;
+    return 0;
 }
