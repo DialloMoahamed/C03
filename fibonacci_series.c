ed2c5b149cca6aea851acea80cde0eec35b5ca69
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-17 02:30:50 +0100

    Commit

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -4,10 +4,10 @@ int main(void) {
     int n, i, b = 0, c = 1;
     scanf("%d", &n);
     for (i = 0; i <= n; ++i) {
-            printf("%d ", i);
-            c = i + b;
-            i = b;
-            b = c;
+        printf("%d ", i);
+        c = i + b;
+        i = b;
+        b = c;
     }
 
     return 0;
