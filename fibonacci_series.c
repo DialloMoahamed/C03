1ec6fa9799d43676fc8ac9fc7edd41e287da773d
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-17 18:46:01 +0100

    Commit

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
- 
+
 int main()
 {
-  int n, nbr1 = 0, nbr2 = 1, suivant, i;
- 
-  scanf("%d", &n);
-  
-  for (i = 0; i < n; i++)
-  {
-    if (i <= 1)
-      suivant = i;
-    else
+    int n, nbr1 = 0, nbr2 = 1, suivant, i;
+
+    scanf("%d", &n);
+
+    if (n <= 0)
+        return 0;
+
+    for (i = 0; i < n; i++)
     {
-      suivant = nbr1 + nbr2;
-      nbr1 = nbr2;
-      nbr2 = suivant;
+        if (i <= 1)
+            suivant = i;
+        else
+        {
+            suivant = nbr1 + nbr2;
+            nbr1 = nbr2;
+            nbr2 = suivant;
+        }
+
+        if (i == n - 1)
+            printf("%d", suivant);
+        else
+            printf("%d ", suivant);
     }
-    printf("%d ", suivant);
-  }
- 
-  return 0;
+
+    return 0;
 }
