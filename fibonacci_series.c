ae6efa4148b7a630bf188a4fb5ff41106aaed357
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-17 18:52:24 +0100

    Commit

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -2,22 +2,27 @@
 
 int main()
 {
-    int limite, a = 0, b = 1, suivant;
+    int limite;
+    int a = 0, b = 1, suivant;
 
     scanf("%d", &limite);
 
     if (limite < 0)
         return 0;
 
-    printf("%d ", a);
+    // Affiche le premier terme sans espace
+    printf("0");
 
+    // Ensuite, on affiche les suivants avec un espace devant
     while (b <= limite)
     {
-        printf("%d ", b);
+        printf(" %d", b);
         suivant = a + b;
         a = b;
         b = suivant;
     }
+
+    printf("\n"); // Fin de ligne obligatoire
 
     return 0;
 }
