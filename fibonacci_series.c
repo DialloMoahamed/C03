5e15045963d9a52319de4587139098965ccc965c
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-17 13:45:25 +0100

    Commit

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -4,14 +4,24 @@ int main(void) {
     int n;
     scanf("%d", &n);
 
-    int a = 0, b = 1;
+    int fib[1000]; // On suppose que 1000 termes suffisent
+    int count = 0;
 
-    for (; a <= n; ) {
-        printf("%d", a);
-        int next = a + b;
-        a = b;
-        b = next;
-        if (a <= n)
+    fib[0] = 0;
+    fib[1] = 1;
+
+    if (n >= 0) count = 1;
+    if (n >= 1) count = 2;
+
+    for (int i = 2; ; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+        if (fib[i] > n) break;
+        count++;
+    }
+
+    for (int i = 0; i < count; i++) {
+        printf("%d", fib[i]);
+        if (i < count - 1)
             printf(" ");
     }
 
