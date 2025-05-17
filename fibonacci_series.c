c0cb0c07f37791b8b4e2077ac5b1e2ee9f33d4e0
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-17 13:42:15 +0100

    Commit

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void) {
     int n;
     scanf("%d", &n);
 
     int a = 0, b = 1;
+    bool first = 1;
 
-    for (; a <= n; ) {
+    while (a <= n) {
+        if (!first) {
+            printf(" ");
+        }
+        first = 0;
         printf("%d", a);
         int next = a + b;
         a = b;
         b = next;
-        if (a <= n)
-            printf(" ");
     }
 
     printf("\n");
