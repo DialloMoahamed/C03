b712da8244db851c7a4272d12723cec48ce8d4a6
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-18 12:42:00 +0100

    Commit

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
-int main(void) {
+// Ce script affiche la série de Fibonacci jusqu'à une limite donnée
+
+int main() {
     int limite;
 
     scanf("%d", &limite);
 
     int a = 0, b = 1, prochainTerme;
 
-    printf("%d, %d, ", a, b);
+    printf("%d %d ", a, b);
 
     prochainTerme = a + b;
 
     while (prochainTerme <= limite) {
-        printf("%d, ", prochainTerme);
+        printf("%d ", prochainTerme);
         a = b;
         b = prochainTerme;
         prochainTerme = a + b;
     }
-    printf("\n");
+
     return 0;
 }
