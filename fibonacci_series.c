7dbe33af186360b4c99b7eefd77158dde8e12f3c
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-18 12:37:25 +0100

    Commit

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
     int limite;
 
     scanf("%d", &limite);
 
     int a = 0, b = 1, prochainTerme;
 
-    printf("%d %d ", a, b);
+    printf("%d, %d, ", a, b);
 
     prochainTerme = a + b;
 
     while (prochainTerme <= limite) {
-        printf("%d ", prochainTerme);
+        printf("%d, ", prochainTerme);
         a = b;
         b = prochainTerme;
         prochainTerme = a + b;
     }
-
+    printf("\n");
     return 0;
 }
