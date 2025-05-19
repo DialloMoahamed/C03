32f120709ed9ce8f8f5553be35cc6e0d028b0a99
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-19 22:44:13 +0100

    Commit 178

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -2,10 +2,9 @@
 
 int main() {
     int limite, premier = 0, second = 1, suivant;
-    printf("Entrez la limite de la série de Fibonacci: ");
     scanf("%d", &limite);
 
-    printf("Série de Fibonacci jusqu'à %d: ", limite);
+    printf("Série de Fibonacci jusqu'à %d : ", limite);
     while (premier <= limite) {
         printf("%d ", premier);
         suivant = premier + second;
