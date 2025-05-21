77e38aa72cfc55984ae1b246d64ee786d9477941
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-21 02:19:49 +0100

    Commit

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -3,25 +3,23 @@
 int main(void)
 {
     unsigned int n;
-    unsigned int fib[100];
-    int i = 2;
+    unsigned int a = 0, b = 1, temp;
 
     scanf("%u", &n);
 
-    fib[0] = 0;
-    fib[1] = 1;
-
-    printf("%u", fib[0]);
-    if (n >= 1)
-        printf(" %u", fib[1]);
+    if (a <= n)
+        printf("%u", a);
+    if (b <= n)
+        printf(" %u", b);
 
     while (1)
     {
-        fib[i] = fib[i - 1] + fib[i - 2];
-        if (fib[i] > n)
+        temp = a + b;
+        if (temp > n)
             break;
-        printf(" %u", fib[i]);
-        i++;
+        printf(" %u", temp);
+        a = b;
+        b = temp;
     }
 
     printf("\n");
