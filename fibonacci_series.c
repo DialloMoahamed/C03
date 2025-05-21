9665607f058c2e5b85a26cf8329996c875e56f60
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-21 01:08:22 +0100

    Commit

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
 
 int main() {
-   unsigned int limite;
-   unsigned int terme1 = 0;
-   unsigned int terme2 = 1;
-   unsigned int suivant;
+    unsigned int limite;
+    unsigned int terme1 = 0, terme2 = 1, suivant;
 
-   scanf("%u", &limite);
-   printf("\n");
+    scanf("%u", &limite);
 
-   if (terme1 <= limite) {
-      printf("%u ", terme1);
-   }
-   if (terme2 <= limite) {
-      printf("%u", terme2);
-   }
+    if (limite >= 0) {
+        printf("%u", terme1);
+    }
 
-   while (1) {
-      suivant = terme1 + terme2;
-      if (suivant > limite) {
-         break;
-      }
-      printf(" %u", suivant);
-      terme1 = terme2;
-      terme2 = suivant;
-   }
+    if (terme2 <= limite) {
+        printf(" %u", terme2);
+    }
 
-   return 0;
+    while (1) {
+        suivant = terme1 + terme2;
+        if (suivant > limite) {
+            break;
+        }
+        printf(" %u", suivant);
+        terme1 = terme2;
+        terme2 = suivant;
+    }
+
+    printf("\n");
+    return 0;
 }
