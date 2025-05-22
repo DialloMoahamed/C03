f385d7e2eff254bfb70b698e234fc01fa3b42014
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-22 01:36:52 +0100

    Commit

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 
-void afficherFibonacci(int limite) {
-    int a = 0, b = 1, suivant;
-    while (a <= limite) {
-        printf("%d ", a);
+int main(void) {
+    unsigned int limite;
+    unsigned int a = 0, b = 1, suivant;
+
+    scanf("%u", &limite);
+
+    if (a <= limite)
+        printf("%u", a);
+
+    if (b <= limite && b != a)
+        printf(" %u", b);
+
+    while (1) {
         suivant = a + b;
+        if (suivant > limite)
+            break;
+        printf(" %u", suivant);
         a = b;
         b = suivant;
     }
+
     printf("\n");
-}
-
-int main() {
-    int limite;
-    scanf("%d", &limite);
-
-    afficherFibonacci(limite);
-
     return 0;
 }
